e908b6cb683952582d9b688ccc039d61f4aaa557
Toast <toast003@tutamail.com>
2024-05-15 23:18:04 +0200

    Aplicar descuento dependiendo de un personaje aleatorio

--- src/app/pagina-tienda/pagina-tienda.component.ts
+++ src/app/pagina-tienda/pagina-tienda.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, numberAttribute } from '@angular/core';
 import { EncabezadoComponent } from '../componentes/encabezado/encabezado.component';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
 import { FooterComponent } from '../componentes/footer/footer.component';
 import { ApiService } from '../servicio/api.service';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-pagina-tienda',
   standalone: true,
-  imports: [EncabezadoComponent, RouterLink, RouterLinkActive, FooterComponent],
+  imports: [EncabezadoComponent, RouterLink, RouterLinkActive, FooterComponent, NgIf],
   templateUrl: './pagina-tienda.component.html',
   styleUrl: './pagina-tienda.component.css'
 })
@@ -18,7 +19,11 @@ export class PaginaTiendaComponent {
   subtitulo = "En colaboración con la sociedad";
 
   textoDescuento = "Tienes un descuento pendiente...";
-  descuento = 6.9;
+  rutaImagen = "assets/img/interrogante.png";
+  
+  // el <p> donde pone el descuento solo aparece cuando el descuento no este vacio
+  // Y el boton desaparece tambien
+  descuento = "vacio";
 
   personajes: any[] = [];
 
@@ -34,6 +39,36 @@ export class PaginaTiendaComponent {
   }
 
   public hacerDescuento() : void {
-    this.textoDescuento = "iouashdiofuahsiodfhasf";
+    var numeroAleatorio = Math.floor(Math.random() * (this.personajes.length) );
+    var personaje = this.personajes[numeroAleatorio];
+
+
+    this.textoDescuento = personaje.name + " te hace un descuento";
+    
+    // Calcular el descuento
+    var temp: number;
+    
+    temp = personaje.id
+    switch (personaje.status) {
+      case "Alive": {
+        temp *= 2;
+        break;
+      }
+      case "Dead": {
+        temp *= 3;
+        break;
+      }
+    }
+
+    if (personaje.gender == "Male" && personaje.location.name == "Earth (Replacement Dimension)") {
+      temp = 42;
+    }
+
+    while (temp > 95) {
+      temp -= 95;
+    }
+
+    this.descuento = temp + "%";
+    this.rutaImagen = personaje.image
   }
 }
